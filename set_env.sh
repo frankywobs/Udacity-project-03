71302c83a33f526d4e4f29e2868c5682c9c51f75
Francis <74510431+frankywobs@users.noreply.github.com>
2022-07-27 01:10:28 -0500

    Set up environment variables

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_USERNAME=francis
+export POSTGRES_PASSWORD=impedance
+export POSTGRES_HOST=wobs.c99tyaqbjjcf.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=wobs
+export AWS_BUCKET=wobbucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=hello
 export URL=http://localhost:8100
